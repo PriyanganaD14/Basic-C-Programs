b1f23e218ee6bdb9a37677e9c7ae9ffee7a994e4
PriyanganaD14 <56598929+PriyanganaD14@users.noreply.github.com>
2020-03-31 23:50:26 +0530

    Add files via upload

---
+++ Chapter1Program6PaperSize.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+int main()
+
+{
+	
+	float A0l,A0b,A1l,A1b,A2l,A2b,A3l,A3b,A4l,A4b,A5l,A5b,A6l,A6b,A7l,A7b,A8l,A8b;
+	
+	A0l=1189;
+	A0b=871;
+	A1l=A0b;
+	A1b=A0l/2;
+	A2l=A1b;
+	A2b=A1l/2;
+	A3l=A2b;
+	A3b=A2l/2;
+	A4l=A3b;
+	A4b=A3l/2;
+	A5l=A4b;
+	A5b=A4l/2;
+	A6l=A5b;
+	A6b=A5l/2;
+	A7l=A6b;
+	A7b=A6l/2;
+	A8l=A7b;
+	A8b=A7l/2;
+	
+	printf(" Print the all paper sizes ");
+	
+	printf("\n Dimension of A0l : %.2f",A0l);
+	printf("\n Dimension of A0b : %.2f",A0b);
+	printf("\n Dimension of A1l : %.2f",A1l);
+	printf("\n Dimension of A1b : %.2f",A1b);
+	printf("\n Dimension of A2l : %.2f",A2l);
+	printf("\n Dimension of A2b : %.2f",A2b);
+	printf("\n Dimension of A3l : %.2f",A3l);
+	printf("\n Dimension of A3b : %.2f",A3b);
+	printf("\n Dimension of A4l : %.2f",A4l);
+	printf("\n Dimension of A4b : %.2f",A4b);
+	printf("\n Dimension of A5l : %.2f",A5l);
+	printf("\n Dimension of A5b : %.2f",A5b);
+	printf("\n Dimension of A6l : %.2f",A6l);
+	printf("\n Dimension of A6b : %.2f",A6b);
+	printf("\n Dimension of A7l : %.2f",A7l);
+	printf("\n Dimension of A7b : %.2f",A7b);
+	printf("\n Dimension of A8l : %.2f",A8l);
+	printf("\n Dimension of A8b : %.2f",A8b);
+	
+	return 0;
+}
