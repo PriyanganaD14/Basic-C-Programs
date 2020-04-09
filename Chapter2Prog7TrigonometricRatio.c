049147c43f7912705308e1514b071ea4e96c1099
PriyanganaD14 <56598929+PriyanganaD14@users.noreply.github.com>
2020-04-09 23:58:23 +0530

    Add files via upload

---
+++ Chapter2Prog7TrigonometricRatio.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	
+	float degree,radian;
+	const float PI=3.14159;
+	
+	printf(" Enter the angle : ");
+	scanf("%f",&degree);
+	
+	radian=degree*(PI/180.0);
+	
+	
+	printf("\n Trigonometric Ration :\n ");
+	
+	printf(" Sin(%f) = %f\n",degree,sin(radian));
+	printf(" Cos(%f) = %f\n",degree,cos(radian));
+	printf(" Tan(%f) = %f\n",degree,tan(radian));
+	printf(" Cosec(%f) = %f\n",degree,1/sin(radian));
+	printf(" Sec(%f) = %f\n",degree,1/cos(radian));
+	printf(" Cot(%f) = %f\n",degree,1/tan(radian));
+	
+	
+	
+	return 0;
+}
