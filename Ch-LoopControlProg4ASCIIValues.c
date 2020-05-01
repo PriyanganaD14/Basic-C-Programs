6351fd9244360974868b3c4e17611e94d24ccd2a
PriyanganaD14 <56598929+PriyanganaD14@users.noreply.github.com>
2020-05-01 22:03:55 +0530

    Add files via upload

---
+++ Ch-LoopControlProg4ASCIIValues.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+	
+	printf("The all ASCII values and their equivqlent characters are \n");
+	
+	while(n>=0 && n<=255)
+	{
+		n==0;
+		printf("%d=%c\n",n,n);
+		n=n+1;
+	}
+	return 0;
+}
